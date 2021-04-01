c8404c2f727a46634c1d40cf5310917be70feb04
Agustin <agustinxmtz@gmail.com>
2021-03-31 21:17:39 -0500

    Finished Refactoring Code

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(session(sess));
+
 
 // turn on routes
 app.use(routes);
